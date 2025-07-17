0755ecf9e4c422ca8e075ba846bd48b3afd695d5
aragami3070 <smirnov17612@gmail.com>
2025-07-17 21:39:34 +0400

    Feat: add find and remove your device ip from nmap-scan-result

--- scripts/nmap-scan.sh
+++ scripts/nmap-scan.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo nmap -sP "$1.*" > ../assets/nmap-scan-result.txt
+# Получение всех своего IP адреса
+ip a | grep "$1" | cut -d '/' -f1 | awk -F' ' '{print $2}' > ../assets/my-ip.txt
+
+# Сканирование сети для нахождения всех IP подключенных к данной сети и исключение своего IP
+sudo nmap -sP "$1.*" | grep -v "$(cat ../assets/my-ip.txt)" > ../assets/nmap-scan-result.txt
+
+rm ../assets/my-ip.txt
