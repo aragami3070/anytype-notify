884f599515309f3dd07e7321fc831fa145d90c99
aragami3070 <smirnov17612@gmail.com>
2025-07-18 02:31:49 +0400

    Feat: add func for get Ip part without number after last dot

--- src/nmap/scanner.rs
+++ src/nmap/scanner.rs
@@ -1,9 +1,22 @@
+use std::io;
+use std::process::{Command, Output};
+
 #[derive(Debug, Clone)]
 pub struct Ip(pub String);
 
-pub fn get_ips() -> Vec<Ip> {
-
-	Vec::new()
+impl Ip {
+	fn get_main_part(&self) -> &str {
+		match self.0.rfind('.').map(|idx| &self.0[..idx]) {
+			Some(s) => s,
+			None => {
+				""
+			}
+		}
+	}
 }
 
+
+pub fn get_ips(router_ip: Ip) -> Result<Vec<Ip>, io::Error> {
+    Ok(Vec::new())
+}
 
