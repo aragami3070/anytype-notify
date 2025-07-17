6e3da2db2208fdb2ca448f6d3814ae3449b70192
aragami3070 <smirnov17612@gmail.com>
2025-07-18 02:39:15 +0400

    Feat: add read LOCAL_IP from .env and use for scan all ips

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod nmap;
+use std::process;
+
+use dotenv::dotenv;
+use nmap::scanner;
+
+use crate::nmap::scanner::Ip;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let local_ip = Ip(std::env::var("LOCAL_IP").expect("LOCAL_IP must be set in .env."));
+
+    println!("local_ip: {}", local_ip.0);
+
+    let list_ip = match scanner::get_ips(local_ip) {
+        Ok(l) => l,
+        Err(message) => {
+            eprintln!("Error: {}", message);
+            process::exit(1);
+        }
+    };
+
+	for ip in list_ip {
+	    println!("ip: {}", ip.0);
+	}
 }
