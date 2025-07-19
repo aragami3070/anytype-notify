d83e69088f62c9a7f78a5bdb9fe438f814738233
aragami3070 <smirnov17612@gmail.com>
2025-07-20 01:54:31 +0400

    Feat: add reload systmd with new enable tunnel.service

--- scripts/socat.sh
+++ scripts/socat.sh
@@ -10,3 +10,20 @@ fi
 if [ ! -f "/etc/systemd/system/tunnel.service" ]; then
 	sudo touch /etc/systemd/system/tunnel.service
 fi
+
+# Настройки сервиса для автозапуска socat при запуске системы
+service="
+[Unit]\n
+Description=Socat tunnel from 0.0.0.0:20390 to localhost:31009\n
+After=network.target\n
+[Service]\n
+ExecStart=/usr/bin/socat TCP-LISTEN:20390,reuseaddr,fork TCP4:localhost:31009\n
+Restart=always\n
+User=root\n
+Group=root\n
+[Install]\n
+WantedBy=multi-user.target\n
+"
+
+# Записываем настройки сервиса
+sudo -i echo -e "$service" > /etc/systemd/system/tunnel.service
