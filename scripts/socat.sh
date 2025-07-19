1bd379cf76cfe79808f720e92c2f2c7d87f82d30
aragami3070 <smirnov17612@gmail.com>
2025-07-20 01:54:06 +0400

    Feat: add touch tunnel.service file if not exists

--- scripts/socat.sh
+++ scripts/socat.sh
@@ -5,3 +5,8 @@ if ! [ -x "$(command -v socat)" ]; then
   echo 'Error: socat is not installed.' >&2
   exit 1
 fi
+
+# Создаем файл с настройками для сервиса
+if [ ! -f "/etc/systemd/system/tunnel.service" ]; then
+	sudo touch /etc/systemd/system/tunnel.service
+fi
