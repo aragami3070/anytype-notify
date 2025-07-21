19ac0a0488fed54bafb1f4aa094889b0c0702257
aragami3070 <smirnov17612@gmail.com>
2025-07-21 16:34:03 +0400

    Feat: add LoginRequest and Identifier for body request to login endpoint

--- src/matrix/api/auth.rs
+++ src/matrix/api/auth.rs
@@ -1,5 +1,25 @@
 use crate::{Password, Token, User, matrix::client::Client};
 
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize)]
+pub struct LoginRequest {
+    identifier: Identifier,
+    #[serde(rename = "initial_device_display_name")]
+    device_name: Option<String>,
+    password: String,
+    #[serde(rename = "type")]
+    login_type: String,
+    #[serde(rename = "refresh_token", skip_serializing_if = "Option::is_none")]
+    request_refresh_token: Option<bool>,
+}
+
+#[derive(Serialize)]
+pub struct Identifier {
+    #[serde(rename = "type")]
+    type_: String,
+    user: String,
+}
 
 pub struct Auth {
     pub client: Client,
@@ -9,5 +29,4 @@ impl Auth {
     pub fn new(client: Client) -> Self {
         Auth { client }
     }
-
 }
