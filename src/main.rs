f131773e98ed71a20fc8e84a63b77b64cdc04696
aragami3070 <smirnov17612@gmail.com>
2025-07-21 20:51:45 +0400

    Feat: login and save matrix tokens in main

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ mod anytype;
 mod matrix;
 
 use serde::{Deserialize, Serialize};
+use std::process;
 
 use anytype::parser::fetch;
 
 use dotenv::dotenv;
 
+use crate::matrix::client;
 
 #[derive(Clone)]
 pub struct Url(pub String);
@@ -31,5 +33,36 @@ async fn main() {
     match fetch(&anytype_url.0, &anytype_token.0).await {
         Ok(_) => println!("Response parsed successfully"),
         Err(e) => println!("Error: {e}"),
+    }
+
+    let matrix_server =
+        Url(std::env::var("MATRIX_SERVER").expect("MATRIX_SERVER must be set in .env."));
+
+    let user = User(std::env::var("MATRIX_USER").expect("MATRIX_USER must be set in .env."));
+    let password =
+        Password(std::env::var("MATRIX_PASSWORD").expect("MATRIX_PASSWORD must be set in .env."));
+
+    let mut matrix_client = match client::Client::new(matrix_server) {
+        Ok(cl) => cl,
+        Err(message) => {
+            eprintln!("Error: {message}");
+            process::exit(1);
+        }
+    };
+
+    matrix_client = match matrix_client.auth().login(user, password).await {
+        Ok(m) => m,
+        Err(message) => {
+            eprintln!("Error: {message}");
+            process::exit(1);
+        }
+    };
+
+    match matrix_client.save_tokens() {
+        Ok(r) => println!("{r}"),
+        Err(message) => {
+            eprintln!("Error: {message}");
+            process::exit(1);
+        }
     }
 }
