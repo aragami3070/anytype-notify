a8e7a8b858720eec60d47b6de54d2bae2d10031b
aragami3070 <smirnov17612@gmail.com>
2025-07-21 16:46:34 +0400

    Feat: add method login to Auth struct

--- src/matrix/api/auth.rs
+++ src/matrix/api/auth.rs
@@ -1,7 +1,8 @@
 use crate::{Password, Token, User, matrix::client::Client};
 
+use reqwest::header::HeaderMap;
 use serde::{Deserialize, Serialize};
-use std::{error::Error};
+use std::error::Error;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct DeviceId(pub String);
@@ -44,5 +45,48 @@ pub struct Auth {
 impl Auth {
     pub fn new(client: Client) -> Self {
         Auth { client }
+    }
+
+    pub async fn login(
+        &self,
+        user: User,
+        password: Password,
+    ) -> Result<LoginResponse, Box<dyn Error>> {
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            "Accept",
+            match "application/json".parse() {
+                Ok(s) => s,
+                Err(message) => return Err(Box::new(message)),
+            },
+        );
+
+        let identifier = Identifier {
+            type_: "m.id.user".to_string(),
+            user: user.0,
+        };
+        let body = LoginRequest {
+            identifier: identifier,
+            device_name: Some("anytype-bot docker".to_string()),
+            password: password.0,
+            login_type: "m.login.password".to_string(),
+            request_refresh_token: Some(true),
+        };
+
+        let response = match self
+            .client
+            .post("/_matrix/client/v3/login", headers, body)
+            .await
+        {
+            Ok(resp) => resp,
+            Err(message) => return Err(message),
+        };
+
+        let result = match response.json::<LoginResponse>().await {
+            Ok(resp) => resp,
+            Err(message) => return Err(Box::new(message)),
+        };
+
+        Ok(result)
     }
 }
