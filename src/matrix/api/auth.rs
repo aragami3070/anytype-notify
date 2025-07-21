d39f5a89c2b452f8a4fe048e5412943da95906c6
aragami3070 <smirnov17612@gmail.com>
2025-07-21 16:43:30 +0400

    Feat: add LoginResponse, DeviceId and UserId Deserialize login response

--- src/matrix/api/auth.rs
+++ src/matrix/api/auth.rs
@@ -1,6 +1,22 @@
 use crate::{Password, Token, User, matrix::client::Client};
 
 use serde::{Deserialize, Serialize};
+use std::{error::Error};
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct DeviceId(pub String);
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct UserId(pub String);
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct LoginResponse {
+    pub access_token: Token,
+    pub device_id: DeviceId,
+    pub user_id: UserId,
+    pub refresh_token: Token,
+    pub expires_in_ms: i64,
+}
 
 #[derive(Serialize)]
 pub struct LoginRequest {
