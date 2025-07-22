4275e6a96d3460ecd3e1f0d04b56e10e0cfde7ee
aragami3070 <smirnov17612@gmail.com>
2025-07-23 02:44:38 +0400

    Feat: add method send_message to Auth struct

--- src/matrix/api/room.rs
+++ src/matrix/api/room.rs
@@ -27,4 +27,38 @@ impl Room {
     pub fn new(client: Client) -> Self {
         Room { client }
     }
+
+    pub async fn send_message(
+        &self,
+        room_id: &RoomId,
+        device_id: &DeviceId,
+        text: String,
+    ) -> Result<EventId, Box<dyn Error>> {
+        let path = format!(
+            "/_matrix/client/v3/rooms/{}/send/m.room.message/{}",
+            room_id.0, device_id.0
+        );
+
+        let mut headers = HeaderMap::new();
+
+        headers.insert("Accept", "application/json".parse()?);
+        headers.insert(
+            "Authorization",
+            format!("Bearer {}", self.client.get_access_token().0).parse()?,
+        );
+
+        let body = MessageBody {
+            body: text,
+            msgtype: "m.text".to_owned(),
+        };
+
+        let response = self
+            .client
+            .put(path.trim(), headers, body)
+            .await?;
+
+		let result = response.json::<EventId>().await?;
+
+        Ok(result)
+    }
 }
