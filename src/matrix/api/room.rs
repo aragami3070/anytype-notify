395e0043da6c616ce9a8e6ca8981ddfb363aad8c
aragami3070 <smirnov17612@gmail.com>
2025-07-23 02:26:19 +0400

    Feat: add MessageBody struct

--- src/matrix/api/room.rs
+++ src/matrix/api/room.rs
@@ -1,4 +1,13 @@
-use crate::matrix::client::Client;
+use serde::Serialize;
+
+use crate::matrix::{api::auth::DeviceId, client::{Client, RoomId}};
+
+
+#[derive(Serialize)]
+pub struct MessageBody {
+    pub body: String,
+    pub msgtype: String,
+}
 
 pub struct Room {
     pub client: Client,
@@ -8,5 +17,4 @@ impl Room {
     pub fn new(client: Client) -> Self {
         Room { client }
     }
-    
 }
