77fa36478ad1e31dd521bd0545570a934a26fb4a
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-08-06 01:31:15 +0400

    fix: add clone attribute

--- src/anytype/entities/notification.rs
+++ src/anytype/entities/notification.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct NotificationObject {
     pub name: String,
     pub snippet: String,
