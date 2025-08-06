924646bd25bed332a8a0688df79b1cb58b9a9965
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-08-07 02:37:05 +0400

    Revert "refactor: remove config.rs"
    This reverts commit 1d56f83507aecf55dea0f4732222d9d9e0bde866.

---
+++ src/config.rs
@@ -0,0 +1,22 @@
+// use crate::RequiredTypes;
+//
+// use serde::Deserialize;
+// use config::Config;
+// use std::error::Error;
+//
+// #[derive(Debug, Clone, Deserialize)]
+// pub struct AppConfig {
+//     pub required_types: RequiredTypes,
+// 	#[allow(dead_code)]
+//     pub interval_minutes: i64,
+// }
+//
+// impl AppConfig {
+//     pub fn from_file(path: &str) -> Result<Self, Box<dyn Error>> {
+//         let config = Config::builder()
+//             .add_source(config::File::with_name(path))
+//             .build()?;
+//
+//         Ok(config.try_deserialize()?)
+//     }
+// }
