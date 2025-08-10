cbb7fa280bb358caaf986c2b58544419c2fe33c7
aragami3070 <smirnov17612@gmail.com>
2025-08-11 01:28:44 +0400

    Feat: add get_matrix_env_vars for dotenv_vars mod

--- src/dotenv_vars.rs
+++ src/dotenv_vars.rs
@@ -20,3 +20,11 @@ pub fn get_anytype_env_vars() -> Result<AnytypeVars, Box<dyn Error>> {
 
     Ok(AnytypeVars { url, token })
 }
+
+pub fn get_matrix_env_vars() -> Result<MatrixVars, Box<dyn Error>> {
+    let server = Url(std::env::var("MATRIX_SERVER")?);
+    let room_id = RoomId(std::env::var("MATRIX_ROOM_ID")?);
+
+    Ok(MatrixVars { server, room_id })
+}
+
