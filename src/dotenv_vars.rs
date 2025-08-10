ee053f161f1dc3bff2eecd922bc9b1c42cbbd0c1
aragami3070 <smirnov17612@gmail.com>
2025-08-11 01:17:54 +0400

    Feat: add get_anytype_env_vars to dotenv_vars mod

--- src/dotenv_vars.rs
+++ src/dotenv_vars.rs
@@ -1,7 +1,19 @@
+use std::error::Error;
+
 use crate::{Token, Url};
 
 #[derive(Debug, Clone)]
 pub struct AnytypeVars {
     pub url: Url,
     pub token: Token,
+}
+
+pub fn get_anytype_env_vars() -> Result<AnytypeVars, Box<dyn Error>> {
+    let url = Url(std::env::var("ANYTYPE_URL")?); // Anytype space URL
+    let token = Token(std::env::var("ANYTYPE_TOKEN")?); // Anytype API token
+	
+	Ok(AnytypeVars{
+		url,
+		token
+	})
 }
